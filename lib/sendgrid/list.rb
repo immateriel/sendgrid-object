1cc9d8597d6b857d101f703fc44a7b49dbdce62b
Elodie Ailleaume <Didi@MacBook-Pro-de-Elodie.local>
2022-02-23 11:19:30 +0100

    Add Sendgrid::List and Sendgrid::Recipient classes.

---
+++ lib/sendgrid/list.rb
@@ -0,0 +1,27 @@
+class List
+  attr_reader :list
+
+  def initialize
+    @api = SendGrid::API.new(api_key: SENDGRID_API_KEY)
+    @list = nil
+  end
+
+  # find a list
+  # @param [Integer] id of the list
+  # @return [Hash]
+  def find(id)
+    response = @api.client.contactdb.lists._(id).get()
+    @list = JSON.parse(response.body)
+  end
+
+  # Add a user to a list
+  # @param [Integer] id of the user
+  # @param [Integer, nil] id of the list
+  # @return [void]
+  def add_recipient(recipient_id, list_id = nil)
+    if recipient_id && (list_id || @list)
+      list_id ||= @list["id"]
+      @api.client.contactdb.lists._(list_id).recipients._(recipient_id).post()
+    end
+  end
+end
---
+++ lib/sendgrid/recipient.rb
@@ -0,0 +1,33 @@
+class Recipient
+  attr_reader :user
+
+  def initialize
+    @api = SendGrid::API.new(api_key: SENDGRID_API_KEY)
+    @user = nil
+  end
+
+  # find a user
+  # @param [String] field name
+  # @param [String] field value
+  # @return [Hash]
+  def find_by(field, value)
+    data = { conditions: [{ and_or: "", field: field, value: value, operator: "eq" }] }
+    response = @api.client.contactdb.recipients.search.post(request_body: data)
+    @user = JSON.parse(response.body)["recipients"]&.first
+  end
+
+  # Create a user and return the id
+  # @param [String] email
+  # @return [String, void] user id
+  def create(email)
+    return @user["id"] if @user && @user["email"] == email
+    if email.present? && existing_user = self.find_by("email", email)
+      @user = existing_user
+      return @user["id"]
+    elsif email.present?
+      data = [{ email: email }]
+      response = @api.client.contactdb.recipients.post(request_body: data)
+      JSON.parse(response.body)["persisted_recipients"]&.first
+    end
+  end
+end
